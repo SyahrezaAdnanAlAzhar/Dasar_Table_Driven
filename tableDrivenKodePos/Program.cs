2adb07c22cdd43862959847800ec18a1056724a5
Syahreza Adnan Al Azhar <notsupersyahreza@student.telkomuniversity.ac.id>
2024-03-12 23:38:42 +0700

    menambahkan implementasi dengan table-driven

--- tableDrivenKodePos/Program.cs
+++ tableDrivenKodePos/Program.cs
@@ -3,11 +3,13 @@ using static KodePos;
 
 public class KodePos
 {
-    public enum Kelurahan { Batununggal, Kujangsari, Mengger, Wates, Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja};
+    public enum Kelurahan { Batununggal, Kujangsari, Mengger, Wates, Cijaura,
+        Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja};
 
     public static int getKodePos(Kelurahan kelurahan)
     {
-        int[] kodePosTiapKelurahan = { 40266 , 40287 , 40267 , 40256 , 40287 , 40286 , 40286 , 40286 , 40272 , 40274 , 40273 };
+        int[] kodePosTiapKelurahan = { 40266 , 40287 , 40267 , 40256 , 40287 ,
+            40286 , 40286 , 40286 , 40272 , 40274 , 40273 };
         return kodePosTiapKelurahan[(int) kelurahan];
     }
 };
